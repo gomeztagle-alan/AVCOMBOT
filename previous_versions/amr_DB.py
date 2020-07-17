db69840d0691891d61d1f8a8334e77ad302dc906
gomeztagle-alan <35383816+gomeztagle-alan@users.noreply.github.com>
2020-07-17 10:00:53 -0500

    new tables(pilot, amr)

--- previous_versions/amr_DB.py
+++ previous_versions/amr_DB.py
@@ -8,11 +8,28 @@ try:
 
     create_pilot_table = """CREATE TABLE pilot (
                         id INTEGER PRIMARY KEY,
-                        pilot TEXT NOT NULL,
+                        discord TEXT NOT NULL,
                         information TEXT NOT NULL,
                         join_date datetime,
                         callsign TEXT NOT NULL,
                         flight_hours INTEGER NOT NULL);"""
 
+    create_amr_table = """CREATE TABLE amr (
+                        id INTEGER PRIMARY KEY,
+                        pilot_id INTEGER FOREIGN KEY REFERENCES pilot(id),
+                        discord TEXT NOT NULL,
+                        information TEXT NOT NULL,
+                        date datetime,
+                        callsign TEXT NOT NULL,
+                        operation TEXT NOT NULL,
+                        """
+    cursor.execute(create_pilot_table)
+    connection.commit()
+    print("Table created")
+
 except sqlite3.Error as error:
-    print("Error while creating ")
+    print("Error while creating table", error)
+finally:
+    if (connection):
+        connection.close()
+        print("sqlite connection is closed")
